959376b5e7ae87a0beab8b5c5ffcdfb54908d22e
mikeymizell <61428335+mikeymizell@users.noreply.github.com>
2022-01-11 14:38:51 -0800

    converted all html to ejs

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.use(routes);
+app.set('view engine', 'ejs');
 
-app.get('/profile', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/profile.html'));
+app.get('/', (req, res) => {
+    res.render('index');
 })
 
-app.get('/recipe-maker', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/recipe.html'))
-})
+// app.get('/profile', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/profile.html'));
+// })
 
-app.get('/recipes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/blog.html'));
-})
+// app.get('/recipe-maker', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/recipe.html'))
+// })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+// app.get('/recipes', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/blog.html'));
+// })
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
